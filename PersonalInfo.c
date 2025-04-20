8cae2c430c07d8a52eeacf03f7d543e112677cce
Ls-cy <48863495+Ls-cy@users.noreply.github.com>
2025-04-20 01:53:39 -0400

    Add files via upload

---
+++ PersonalInfo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // Print information for Unit Test #1
+    printf("Lucinda Potter\n");
+    printf("6/24/1992\n");
+    printf("work 000-000-0101\n");
+    printf("cell 000-000-0189\n");
+
+    // Print information for Unit Test #2
+    printf("John Smith\n");
+    printf("8/15/1978\n");
+    printf("work 000-111-0101\n");
+    printf("cell 000-111-0189\n");
+
+    // Print information for Unit Test #3
+    printf("Chris Johnson\n");
+    printf("12/01/1987\n");
+    printf("work 000-222-0101\n");
+    printf("cell 000-222-0189\n");
+
+    // Print information for Unit Test #4
+    printf("Robert Hall\n");
+    printf("2/27/1949\n");
+    printf("work 000-444-0101\n");
+    printf("cell 000-444-0189\n");
+
+    return 0;
+}
